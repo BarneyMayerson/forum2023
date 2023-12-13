75aace876d1f15092cf8ee319504be49c0b51580
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-12-13 15:07:11 +0300

    08 Pagination (VI)

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,18 +1,21 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
+import Pagination from "@/Components/Pagination.vue";
 
 defineProps(["posts"]);
 </script>
 
 <template>
-    <AppLayout>
-        <Container>
-            <ul class="divide-y dark:divide-gray-700">
-                <li v-for="post in posts" :key="post.id" class="px-2 py-3">
-                    <span class="font-semibold text-lg">{{ post.title }}</span>
-                </li>
-            </ul>
-        </Container>
-    </AppLayout>
+  <AppLayout>
+    <Container>
+      <ul class="divide-y dark:divide-gray-700">
+        <li v-for="post in posts.data" :key="post.id" class="px-2 py-3">
+          <span class="font-semibold text-lg">{{ post.title }}</span>
+        </li>
+      </ul>
+
+      <Pagination :meta="posts.meta" />
+    </Container>
+  </AppLayout>
 </template>
