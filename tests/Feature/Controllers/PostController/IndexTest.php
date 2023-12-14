b15d76a664393d05b620bd6684046144e0cd4806
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-12-14 15:22:13 +0300

    10 Showing a Single Post

--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -2,17 +2,12 @@
 
 use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Http\Resources\Json\ResourceCollection;
-use Inertia\Testing\AssertableInertia;
 
 use function Pest\Laravel\get;
 
 it('should return the correct component', function() {
     get(route('posts.index'))
-        ->assertInertia(fn(AssertableInertia $inertia) => $inertia
-            ->component('Posts/Index', true)
-        );
+        ->assertComponent('Posts/Index');
 });
 
 it('passes posts to the view', function() {
