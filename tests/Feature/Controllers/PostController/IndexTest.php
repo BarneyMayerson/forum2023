2491b2d6a1dd68250dd35246008e4610cd5b3db9
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-12-14 10:58:19 +0300

    09 Helpful Testing Helpers (part 02) (VI)

--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -3,6 +3,7 @@
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 use Inertia\Testing\AssertableInertia;
 
 use function Pest\Laravel\get;
@@ -28,10 +29,26 @@ it('passes posts to the view', function() {
         return $this;
     });
 
+    AssertableInertia::macro('hasPaginatedResource', function(string $key, ResourceCollection $resource) {
+        $props = $this->toArray()['props'];
+
+        $compiledResource = $resource->response()->getData(true);
+
+        expect($props)
+            ->toHaveKey($key, message: "Key \"{$key}\" not passed as a property to Inertia.")
+            ->and($props[$key])
+            ->toHaveKeys(['data', 'meta', 'links'])
+            ->data
+            ->toEqual($compiledResource);
+
+        return $this;
+    });
+
     $posts = Post::factory(3)->create();
 
     get(route('posts.index'))
         ->assertInertia(fn(AssertableInertia $inertia) => $inertia
             ->hasResource('post', PostResource::make($posts->first()))
+            ->hasPaginatedResource('posts', PostResource::collection($posts->reverse()))
         );
 });
