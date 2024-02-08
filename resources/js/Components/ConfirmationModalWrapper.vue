f0e64f407b1c9eff212af2e4bf93c60b40308672
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-02-08 11:20:31 +0300

    23 Useful User Feedback: Confirming Actions (part 02) focus on modal button [VI]

--- resources/js/Components/ConfirmationModalWrapper.vue
+++ resources/js/Components/ConfirmationModalWrapper.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { nextTick, ref, watchEffect } from "vue";
 import ConfirmationModal from "@/Components/ConfirmationModal.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { useConfirm } from "@/Composables/useConfirm";
 
 const { state, confirm, cancel } = useConfirm();
+
+const cancelButtonRef = ref(null);
+
+watchEffect(async () => {
+  if (state.show) {
+    await nextTick();
+
+    cancelButtonRef.value?.$el.focus();
+  }
+});
 </script>
 
 <template>
@@ -14,7 +25,9 @@ const { state, confirm, cancel } = useConfirm();
     <template #content>{{ state.message }}</template>
 
     <template #footer>
-      <SecondaryButton @click="cancel">Cancel</SecondaryButton>
+      <SecondaryButton ref="cancelButtonRef" @click="cancel">
+        Cancel
+      </SecondaryButton>
       <PrimaryButton @click="confirm" class="ml-4">Confirm</PrimaryButton>
     </template>
   </ConfirmationModal>
