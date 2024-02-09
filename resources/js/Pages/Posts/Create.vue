83f39ab1f016155b07519a39152c47edd8d9f2fb
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-02-09 10:51:44 +0300

    25 Creating Posts: the Frontend Edition

---
+++ resources/js/Pages/Posts/Create.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Container from "@/Components/Container.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import TextArea from "@/Components/TextArea.vue";
+import InputError from "@/Components/InputError.vue";
+
+const form = useForm({
+  title: "",
+  body: "",
+});
+
+const createPost = () => form.post(route("posts.store"));
+</script>
+
+<template>
+  <AppLayout title="Create a Post">
+    <Container>
+      <h1 class="text-2xl font-bold">Create a Post</h1>
+
+      <form @submit.prevent="createPost" class="mt-3 space-y-3">
+        <div class="">
+          <InputLabel for="title" class="sr-only">Title</InputLabel>
+          <TextInput
+            v-model="form.title"
+            id="title"
+            class="w-full"
+            placeholder="Give it a create title..."
+          />
+          <InputError :message="form.errors.title" class="mt-1" />
+        </div>
+        <div class="">
+          <InputLabel for="body" class="sr-only">Body</InputLabel>
+          <TextArea v-model="form.body" id="body" rows="20" />
+          <InputError :message="form.errors.body" class="mt-1" />
+        </div>
+        <div class="">
+          <PrimaryButton type="submit">Create Post</PrimaryButton>
+        </div>
+      </form>
+    </Container>
+  </AppLayout>
+</template>
