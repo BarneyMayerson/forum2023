a06b945b23190679417567e340d431c563ffab43
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-02-20 11:27:30 +0300

    29 I: A New Editor (part 02) Markdown

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -2,6 +2,7 @@
 import { EditorContent, useEditor } from "@tiptap/vue-3";
 import { StarterKit } from "@tiptap/starter-kit";
 import { watch } from "vue";
+import { Markdown } from "tiptap-markdown";
 
 const props = defineProps({
   modelValue: "",
@@ -10,27 +11,35 @@ const props = defineProps({
 const emit = defineEmits(["update:modelValue"]);
 
 const editor = useEditor({
-  extensions: [StarterKit],
-  onUpdate: () => emit("update:modelValue", editor.value?.getHTML()),
+  extensions: [StarterKit, Markdown],
+
+  editorProps: {
+    attributes: {
+      class: "min-h-[512px] prose prose-sm max-w-none py-1.5 px-3",
+    },
+  },
+
+  onUpdate: () =>
+    emit("update:modelValue", editor.value?.storage.markdown.getMarkdown()),
 });
 
 watch(
   () => props.modelValue,
   (value) => {
-    if (value === editor.value?.getHTML()) {
+    if (value === editor.value?.storage.markdown.getMarkdown()) {
       return;
     }
 
     editor.value?.commands.setContent(value);
   },
-  {
-    immediate: true,
-  },
+  { immediate: true },
 );
 </script>
 
 <template>
-  <div class="">
+  <div
+    class="bg-white rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+  >
     <EditorContent :editor="editor" />
   </div>
 </template>
