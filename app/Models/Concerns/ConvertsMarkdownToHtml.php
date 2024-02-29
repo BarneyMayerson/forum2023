eb3ba1c539c10103a55b86fbfa774787cbcba344
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-02-29 15:31:14 +0300

    32 IV: Return of the HTML (part 03) more generic trait

--- app/Models/Concerns/ConvertsMarkdownToHtml.php
+++ app/Models/Concerns/ConvertsMarkdownToHtml.php
@@ -6,14 +6,23 @@ trait ConvertsMarkdownToHtml
 {
     protected static function bootConvertsMarkdownToHtml()
     {
-        static::saving(
-            fn(self $model) => $model->fill([
-                "html" => Str($model->body)->markdown([
+        static::saving(function (self $model) {
+            $markdownData = collect(self::getMarkdownToHtmlMap())->flip()->map(
+                fn($bodyColumn) => str($model->$bodyColumn)->markdown([
                     "html_input" => "strip",
                     "allow_unsafe_links" => false,
                     "max_nesting_level" => 5,
-                ]),
-            ])
-        );
+                ])
+            );
+
+            return $model->fill($markdownData->all());
+        });
+    }
+
+    protected static function getMarkdownToHtmlMap(): array
+    {
+        return [
+            "body" => "html",
+        ];
     }
 }
