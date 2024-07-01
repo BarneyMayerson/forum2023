95bc8f37653d0eccd68c3bf4cefe9abfedfe5a2c
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-07-01 10:06:17 +0300

    49 Fixing tests

--- tests/Feature/Controllers/PostController/ShowTest.php
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -19,7 +19,7 @@ it("passes a post to the view", function () {
 
     get($post->showRoute())->assertHasResource(
         "post",
-        PostResource::make($post)
+        PostResource::make($post)->withLikePermission()
     );
 });
 
@@ -28,9 +28,14 @@ it("passes comments to the view", function () {
     $comments = Comment::factory(2)->for($post)->create();
     $comments->load("user");
 
+    $expectedResource = CommentResource::collection($comments->reverse());
+    $expectedResource->collection->transform(
+        fn(CommentResource $resource) => $resource->withLikePermission()
+    );
+
     get($post->showRoute())->assertHasPaginatedResource(
         "comments",
-        CommentResource::collection($comments->reverse())
+        $expectedResource
     );
 });
 
